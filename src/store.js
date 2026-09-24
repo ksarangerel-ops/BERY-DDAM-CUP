121ddd74182f5e1b20f26837005fdac5720a6996
BERY <k.sarangerel@mn.data-artist.com>
2026-09-24 16:15:34 +0800

    Restore default team names and tags

--- src/store.js
+++ src/store.js
@@ -15,14 +15,18 @@
 import { TEAM_SEED, SQUAD_SIZE, NUM_TEAMS, CACHE_KEY } from './config.js';
 import * as realtime from './supabase.js';
 
-const TEAM_NAME_VERSION = 'ganaa-team-names-v1';
+const TEAM_NAME_VERSION = 'ganaa-team-names-v2';
 
 function migrateTeamNames(state) {
   if (!state || state.teamNameVersion === TEAM_NAME_VERSION) return state;
   return {
     ...state,
     teamNameVersion: TEAM_NAME_VERSION,
-    teams: state.teams.map((team, index) => ({ ...team, name: TEAM_SEED[index]?.[0] || team.name })),
+    teams: state.teams.map((team, index) => ({
+      ...team,
+      name: TEAM_SEED[index]?.[0] || team.name,
+      tag: TEAM_SEED[index]?.[1] || team.tag,
+    })),
   };
 }
 
